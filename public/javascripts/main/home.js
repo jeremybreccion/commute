3b938896251232b8e43929f772cac2cfda043ffd
jeremybreccion <34333326+jeremybreccion@users.noreply.github.com>
2018-03-28 10:23:13 +0800

    can search guides at home page

--- public/javascripts/main/home.js
+++ public/javascripts/main/home.js
@@ -1,2 +1,13 @@
-app.controller('HomeController', function($scope, $rootScope){
+app.controller('HomeController', function($scope, $http, $rootScope){
+    $scope.search = function(){
+        var searchObject = {
+            from: $scope.search.from,
+            to: $scope.search.to
+        }
+        $http.post('/posts/getPosts', searchObject).then(function(res){
+            $scope.posts = res.data;
+        }).catch(function(err){
+            $scope.message = err.data.message;
+        });
+    }
 });
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -16,4 +16,24 @@ router.post('/add', function(req, res, next){
     });
 });
 
+//for search results. can have more than 1
+//POST method is use to get posts because req.body must be used
+//req.body must be used because express.jwt.unless does not exempt url routes with params
+//need to exempt route so user is not required to login when searching
+router.post('/getPosts', function(req, res, next){
+    //{ title: {from: req.params.from, to: req.params.to}} does not work
+    console.log(req.body);
+    db.posts.find({'title.from': req.body.from, 'title.to': req.body.to}).toArray(function(err, posts){
+        if(err){
+            res.status(400).send({message: 'Error in searching'});
+        }
+        else if(posts.length == 0){
+            res.status(400).send({message: 'No results found'});
+        }
+        else{
+            res.status(200).send(posts);
+        }
+    });
+});
+
 module.exports = router;
